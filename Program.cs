7a03c87edab1da7fc3e24cf05cfc7e53d9512d97
Vinicius De Martin <demartinvinicius@gmail.com>
2023-03-02 10:17:57 -0300

    Generic parameters and generic request created

--- Program.cs
+++ Program.cs
@@ -1,9 +1,16 @@
 using BogusHost;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using TheRobot;
+using TheRobot.DriverService;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
         services.AddHostedService<Worker>();
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.Load("TheRobot")));
+        services.AddSingleton<WebDriverService>();
+        services.AddSingleton<Robot>();
     })
     .Build();
 
--- Worker.cs
+++ Worker.cs
@@ -1,16 +1,31 @@
+using TheRobot;
+using TheRobot.MediatedRequests;
+using TheRobot.WebRequestsParameters;
+
 namespace BogusHost;
 
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
+    private readonly Robot _robot;
 
-    public Worker(ILogger<Worker> logger)
+    public Worker(ILogger<Worker> logger, Robot robot)
     {
         _logger = logger;
+        _robot = robot;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        await _robot.Exec3Async(new MediatedNavigationRequest()
+        {
+            Parameters = new NavigateRequestParameters()
+            {
+                Url = "http://www.uol.com"
+            }
+        });
+
+        //await _robot.Exec3Async(new MediatedQuitDriverRequest());
         while (!stoppingToken.IsCancellationRequested)
         {
             _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
